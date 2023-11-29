26ee77469370461fd8d169f35bd3799f057a8b59
Doris-Zhu <70694835+Doris-Zhu@users.noreply.github.com>
2023-11-29 17:10:29 +0000

    implemented Player class

--- dominion.py
+++ dominion.py
@@ -1,3 +1,5 @@
+import random
+
 class Card:
     def __init__(self, name, cost, card_type, value=0):
         self.name = name
@@ -29,22 +31,68 @@ class Player:
                 self.shuffle_discard_into_deck()
 
     def shuffle_discard_into_deck(self):
-        # Shuffle the discard pile and add it to the deck
-        pass
+        random.shuffle(self.discard_pile)
+        self.deck.extend(self.discard_pile)
+        self.discard_pile.clear()
 
-    def play_action_card(self, card):
-        # Implement card effects
-        card.effect(self, game)
+    def play_action_card(self, card, game):
+        # Check if the player has actions available
+        if self.actions > 0:
+            # Ensure the card is in the hand and is an action card
+            if card in self.hand and card.card_type == 'Action':
+                # Play the card
+                card.effect(self, game)
+
+                # Move the card from hand to discard pile
+                self.hand.remove(card)
+                self.discard_pile.append(card)
+
+                # Decrease the number of available actions
+                self.actions -= 1
+            else:
+                print("Cannot play this card")
+        else:
+            print("No more actions left")
 
     def play_treasure_cards(self):
-        # Automatically play all treasure cards in hand
-        pass
+        for card in self.hand[:]:  # Iterate over a copy of the hand
+            if card.card_type == 'Treasure':
+                # Play the card
+                card.effect(self, None)  # Assuming the game object is not needed for treasures
 
-    def buy_card(self, card):
-        pass
+                # Move the card from hand to discard pile
+                self.hand.remove(card)
+                self.discard_pile.append(card)
+
+    def buy_card(self, card, game):
+        # Check if the card is available in the supply and if the player has enough coins and buys
+        if game.supply.get(card.name, 0) > 0 and self.coins >= card.cost and self.buys > 0:
+            # Deduct the cost of the card from the player's coins
+            self.coins -= card.cost
+
+            # Decrease the number of buys
+            self.buys -= 1
+
+            # Move the card from the supply to the player's discard pile
+            self.discard_pile.append(card)
+
+            # Decrease the card count in the supply
+            game.supply[card.name] -= 1
+        else:
+            print("Cannot buy this card")
 
     def end_turn(self):
-        pass
+        # Move all cards from hand to discard pile
+        self.discard_pile.extend(self.hand)
+        self.hand.clear()
+
+        # Reset the player's actions, buys, and coins
+        self.actions = 1
+        self.buys = 1
+        self.coins = 0
+
+        # Draw a new hand for the next turn
+        self.draw_hand()
 
 
 class DominionGame:
